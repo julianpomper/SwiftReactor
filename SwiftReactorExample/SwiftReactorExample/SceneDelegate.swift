d56f6927ba16a1e266bc49e198345afda63ecfe9
Dominik Arnhof <da@tailored-apps.com>
2021-11-23 13:56:39 +0100

    add ReactorView for reactor lifecycle handling

--- SwiftReactorExample/SwiftReactorExample/SceneDelegate.swift
+++ SwiftReactorExample/SwiftReactorExample/SceneDelegate.swift
@@ -22,11 +22,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
             .environmentObject(ExampleReactor())
+        
+        let navigationView = NavigationView {
+            contentView
+        }
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: navigationView)
             self.window = window
             window.makeKeyAndVisible()
         }
